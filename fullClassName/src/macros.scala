b77618adaa296486237545dc032aa7db46b5d0aa
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-06-15 10:55:36 +0200

    Remove unneeded import

--- fullClassName/src/macros.scala
+++ fullClassName/src/macros.scala
@@ -6,5 +6,4 @@ inline def fullClassName[T]: String = ${ fullClassNameImpl[T] }
 
 def fullClassNameImpl[T](using qctx: QuoteContext,
   tpe: Type[T]): Expr[String] =
-  import qctx.tasty._
   Expr(tpe.unseal.symbol.fullName)

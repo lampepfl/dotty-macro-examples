2f14e88dbb3f888a9dcebd56fb15de67138d696f
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-12-17 11:35:11 +0100

    Clarify example

--- referenceVariableFromOtherExpr/src/macro.scala
+++ referenceVariableFromOtherExpr/src/macro.scala
@@ -10,17 +10,17 @@ def dummyImpl(using Quotes): Expr[Any] =
   import quotes.reflect.*
   val stuffDef = '{ val stuff = StateContainer("FoobBar") }
   stuffDef.asTerm match
-    case Inlined(_, _, Block(valDef :: Nil, _)) =>
-      val stuffDefStat = valDef.asInstanceOf[Statement]  // This statement defines `val` and needs to be
-                                                         // eventually in scope at the site we use `val`
-      val stuffRef = Ref(stuffDefStat.symbol)            // This is a reference to `val` via which we can
-                                                         // use it later
+    case Inlined(_, _, Block((valDef: Statement) :: Nil, _)) =>
+      val stuffDefStat = valDef                // This statement defines `val` and needs to be
+                                               // eventually in scope at the site we use `val`
+      val stuffRef = Ref(stuffDefStat.symbol)  // This is a reference to `val` via which we can
+                                               // use it later
 
-      val exprThatWorksOnValueItDoesntDefine: Expr[Unit] = '{ ({(stuff: StateContainer) =>
-        stuff.value = "Hello World"
-        println(stuff.value)
-      })(${stuffRef.asExprOf[StateContainer]}) }
+      val stuffRefExpr = stuffRef.asExprOf[StateContainer]
+      val exprThatWorksOnValueItDoesntDefine: Expr[Unit] = '{
+        $stuffRefExpr.value = "Hello World"
+        println($stuffRefExpr.value)
+      }
 
       val result = Block(stuffDefStat :: Nil, exprThatWorksOnValueItDoesntDefine.asTerm).asExpr
-      println(result.show)
       result
